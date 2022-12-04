73f81b3db0c0febf6b44d0492f27bc01455f0855
liljayr <liljayr@gmail.com>
2022-12-04 13:13:47 +0100

    moving dataset to hpc

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data_dir = '../LibriMix/storage_dir/LibriSpeech/dev-clean/84/121123/'
+data_dir = '../LibriMix/storage_dir/LibriSpeech/dev-clean/8842/302196/'
 # df1=pd.read_csv(data_dir+'train_tp.csv')
 # show = df1.iloc[0]
 # df1.head()
 # 'LibriMix/storage_dir/LibriSpeech/dev-clean/84/121123/84-121123-0000.flac'
 
 
-data, samplerate = sf.read(data_dir+'84-121123-0000.flac')
+data, samplerate = sf.read(data_dir+'8842-302196-0000.flac')
 times = np.linspace(0,len(data),len(data))/samplerate
+print(len(np.unique(data)))
 plt.plot(times,data)
 plt.xlim(1.0, 2.0)
 plt.ylim(-.5,.5)
